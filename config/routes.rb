654d94b44978e95a24f87b730c788b5dcefa2b39
DuchSamai <duchsamai77@gamil.com>
2019-06-14 13:58:10 +0700

    user login

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
+  resources :roles
   devise_for :users, path: 'auth'
   root to: 'home#index'
   
   namespace :admin do
     get '/dashboard' => 'dashboard#index'
-    resources :users, except: [:show]
+    resources :users
   end
 end
