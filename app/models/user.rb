e7bf78d72dea7cca44bb648ddffe7d92d26923ba
SovannthaiNEANG <sovannthaineang@gmail.com>
2019-06-14 15:48:25 +0700

    category article

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   
   belongs_to :role
-  has_many
+  has_many :articles
   validates :email, :role, presence: true
 
 end
